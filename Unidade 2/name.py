277530d3f4c49db055111ff8c1ac581f41b7f06e
Bruno Brito <72562028+fox-afk@users.noreply.github.com>
2024-03-12 19:53:28 -0300

    att

--- Unidade 2/name.py
+++ Unidade 2/name.py
@@ -1,5 +1,11 @@
-name = "ada lovelace"
+first_name = "ada "
 
-print(name.title()) # Ada Lovelace
-print(name.upper()) # ADA LOVELACE
-print(name.lower()) # ada lovelace
+# print(name.title()) # Ada Lovelace
+# print(name.upper()) # ADA LOVELACE
+# print(name.lower()) # ada lovelace
+
+last_name="lovelace"
+
+full_name= first_name + "" + last_name
+
+print(full_name)  # ada lovelace
