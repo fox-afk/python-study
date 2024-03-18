ae3f712d804ddaf286a822989e98704506c0d93d
Bruno Brito <72562028+fox-afk@users.noreply.github.com>
2024-03-18 12:35:43 -0300

    att

---
+++ unidade 5/exercicios.py
@@ -0,0 +1,27 @@
+# 5.1 – Testes condicionais: Escreva uma série de testes condicionais. Exiba uma
+# frase que descreva o teste e o resultado previsto para cada um. Seu código
+#deverá ser semelhante a: car = 'subaru'
+
+#       print("Is car == 'subaru'? I predict True."): print(car == 'subaru')
+#       print("\nIs car == 'audi'? I predict False."): print(car == 'audi') 
+
+# • Observe atentamente seus resultados e certifique-se de que compreende por que cada linha é avaliada como True ou False.
+# • Crie pelo menos dez testes. Tenha no mínimo cinco testes avaliados como True e outros cinco avaliados como False.
+car = 'subaru'
+
+
+print("Is car ",car== 'audi',"?")
+print("\nIs",car=='subaru',"?")
+
+
+# 5.2 – Mais testes condicionais: Você não precisa limitar o número de testes que criar em dez. 
+# Se quiser testar mais comparações, escreva outros testes e acrescente-os em conditional_tests.py.
+# Tenha pelo menos um resultado True e umFalse para cada um dos casos a seguir: 
+
+#                                           • testes de igualdade e de não igualdade com strings; 
+#                                           • testes usando a função lower();
+#                                           • testes numéricos queenvolvam igualdade e não igualdade, 
+#                                              maior e menor que, maior ou igual a e menor ou igual a; 
+#                                           • testes usando as palavras reservadas and e or; 
+#                                           • testes para verificar se um item está em uma lista; 
+#                                           • testes para verificar se um item não está em uma lista.
