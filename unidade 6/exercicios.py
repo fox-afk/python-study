60a4556edf92df99821d9fc0e6ed431adfd6acb1
Bruno Brito <72562028+fox-afk@users.noreply.github.com>
2024-03-19 13:38:51 -0300

    add

---
+++ unidade 6/exercicios.py
@@ -0,0 +1,81 @@
+# 6.1 – Pessoa: Use um dicionário para armazenar informações sobre uma pessoa
+#que você conheça. Armazene seu primeiro nome, o sobrenome, a idade e a
+#cidade em que ela vive. Você deverá ter chaves como first_name, last_name,
+#age e city. Mostre cada informação armazenada em seu dicionário.
+
+#6.2 – Números favoritos: Use um dicionário para armazenar os números favoritos
+# de algumas pessoas. Pense em cinco nomes e use-os como chaves em seu
+# dicionário. Pense em um número favorito para cada pessoa e armazene cada
+# um como um valor em seu dicionário. Exiba o nome de cada pessoa e seu
+# número favorito. Para que seja mais divertido ainda, faça uma enquete com
+#alguns amigos e obtenha alguns dados reais para o seu programa.
+
+# 6.3 – Glossário: Um dicionário Python pode ser usado para modelar um
+#dicionário de verdade. No entanto, para evitar confusão, vamos chamá-lo de
+#glossário.
+
+# • Pense em cinco palavras relacionadas à programação que você conheceu nos capítulos anteriores. Use essas palavras como chaves em seu glossário e armazene seus significados como valores.
+# • Mostre cada palavra e seu significado em uma saída formatada de modo elegante. Você pode exibir a palavra seguida de dois-pontos e depois o seu
+# significado, ou apresentar a palavra em uma linha e então exibir seu significado indentado em uma segunda linha. Utilize o caractere de quebra
+# de linha (\n) para inserir uma linha em branco entre cada par palavrasignificado em sua saída
+
+# 6.4 – Glossário 2: Agora que você já sabe como percorrer um dicionário com
+# um laço, limpe o código do Exercício 6.3 (página 148), substituindo sua
+# sequência de instruções print por um laço que percorra as chaves e os valores
+# do dicionário. Quando tiver certeza de que seu laço funciona, acrescente mais
+# cinco termos de Python ao seu glossário. Ao executar seu programa novamente,
+#essas palavras e significados novos deverão ser automaticamente incluídos na
+# saída.
+# 6.5 – Rios: Crie um dicionário que contenha três rios importantes e o país que
+# cada rio corta. Um par chave-valor poderia ser 'nilo': 'egito'.
+
+# • Use um laço para exibir uma frase sobre cada rio, por exemplo, O Nilo corre pelo Egito.
+# • Use um laço para exibir o nome de cada rio incluído no dicionário.
+# • Use um laço para exibir o nome de cada país incluído no dicionário.
+
+# 6.6 – Enquete: Utilize o código em favorite_languages.py (página 150).
+
+# • Crie uma lista de pessoas que devam participar da enquete sobre linguagem
+# favorita. Inclua alguns nomes que já estejam no dicionário e outros que não estão.
+
+# • Percorra a lista de pessoas que devem participar da enquete. Se elas já
+#tiverem respondido à enquete, mostre uma mensagem agradecendo-lhes por
+#responder. Se ainda não participaram da enquete, apresente uma mensagem
+#convidando-as a responder.
+
+
+# 6.7 – Pessoas: Comece com o programa que você escreveu no Exercício 6.1
+# (página 147). Crie dois novos dicionários que representem pessoas diferentes e
+# armazene os três dicionários em uma lista chamada people. Percorra sua lista
+# de pessoas com um laço. À medida que percorrer a lista, apresente tudo que
+# você sabe sobre cada pessoa.
+
+#6.8 – Animais de estimação: Crie vários dicionários, em que o nome de cada
+# dicionário seja o nome de um animal de estimação. Em cada dicionário, inclua
+# o tipo do animal e o nome do dono. Armazene esses dicionários em uma lista
+# chamada pets. Em seguida, percorra sua lista com um laço e, à medida que
+# fizer isso, apresente tudo que você sabe sobre cada animal de estimação.
+
+# 6.9 – Lugares favoritos: Crie um dicionário chamado favorite_places. Pense em
+# três nomes para usar como chaves do dicionário e armazene de um a três
+# lugares favoritos para cada pessoa. Para deixar este exercício um pouco mais
+# interessante, peça a alguns amigos que nomeiem alguns de seus lugares
+# favoritos. Percorra o dicionário com um laço e apresente o nome de cada
+# pessoa e seus lugares favoritos.
+
+# 6.10 – Números favoritos: Modifique o seu programa do Exercício 6.2 (página
+# 147) para que cada pessoa possa ter mais de um número favorito. Em seguida,
+# apresente o nome de cada pessoa, juntamente com seus números favoritos.
+
+# 6.11 – Cidades: Crie um dicionário chamado cities. Use os nomes de três
+# cidades como chaves em seu dicionário. Crie um dicionário com informações
+# sobre cada cidade e inclua o país em que a cidade está localizada, a
+# população aproximada e um fato sobre essa cidade. As chaves do dicionário
+# de cada cidade devem ser algo como country, population e fact. Apresente o
+# nome de cada cidade e todas as informações que você armazenou sobre ela.
+
+# 6.12 – Extensões: Estamos trabalhando agora com exemplos complexos o
+# bastante para poderem ser estendidos de várias maneiras. Use um dos
+# programas de exemplo deste capítulo e estenda-o acrescentando novas chaves
+# e valores, alterando o contexto do programa ou melhorando a formatação da
+# saída.
---
+++ unidade 7/exercicio.py
@@ -0,0 +1,53 @@
+# 7.1 – Locação de automóveis: Escreva um programa que pergunte ao usuário
+# qual tipo de carro ele gostaria de alugar. Mostre uma mensagem sobre esse
+# carro, por exemplo, “Deixe-me ver se consigo um Subaru para você.”
+
+# 7.2 – Lugares em um restaurante: Escreva um programa que pergunte ao usuário
+# quantas pessoas estão em seu grupo para jantar. Se a resposta for maior que
+# oito, exiba uma mensagem dizendo que eles deverão esperar uma mesa. Caso
+# contrário, informe que sua mesa está pronta.
+
+# 7.3 – Múltiplos de dez: Peça um número ao usuário e, em seguida, informe se o
+# número é múltiplo de dez ou não
+
+# 7.4 – Ingredientes para uma pizza: Escreva um laço que peça ao usuário para
+# fornecer uma série de ingredientes para uma pizza até que o valor 'quit' seja
+# fornecido. À medida que cada ingrediente é especificado, apresente uma
+# mensagem informando que você acrescentará esse ingrediente à pizza.
+
+# 7.5 – Ingressos para o cinema: Um cinema cobra preços diferentes para os
+# ingressos de acordo com a idade de uma pessoa. Se uma pessoa tiver menos
+# de 3 anos de idade, o ingresso será gratuito; se tiver entre 3 e 12 anos, o
+# ingresso custará 10 dólares; se tiver mais de 12 anos, o ingresso custará 15
+# dólares. Escreva um laço em que você pergunte a idade aos usuários e, então,
+# informe-lhes o preço do ingresso do cinema.
+
+# 7.6 – Três saídas: Escreva versões diferentes do Exercício 7.4 ou do Exercício
+# 7.5 que faça o seguinte, pelo menos uma vez: 
+#   • use um teste condicional na instrução while para encerrar o laço; 
+#   • use uma variável active para controlar o tempo que o laço executará;
+#   • use uma instrução break para sair do laço quando o usuário fornecer o valor 'quit'.
+
+# 7.7 – Infinito: Escreva um laço que nunca termine e execute-o. (Para encerrar o
+# laço, pressione CTRL-C ou feche a janela que está exibindo a saída.)
+
+# 7.8 – Lanchonete: Crie uma lista chamada sandwich_orders e a preencha com
+# os nomes de vários sanduíches. Em seguida, crie uma lista vazia chamada
+# finished_sandwiches. Percorra a lista de pedidos de sanduíches com um laço e
+# mostre uma mensagem para cada pedido, por exemplo, Preparei seu
+# sanduíche de atum. À medida que cada sanduíche for preparado, transfira-o
+# para a lista de sanduíches prontos. Depois que todos os sanduíches estiverem
+# prontos, mostre uma mensagem que liste cada sanduíche preparado.
+
+# 7.9 – Sem pastrami: Usando a lista sandwich_orders do Exercício 7.8, garanta
+# que o sanduíche de 'pastrami' apareça na lista pelo menos três vezes.
+# Acrescente um código próximo ao início de seu programa para exibir uma
+# mensagem informando que a lanchonete está sem pastrami e, então, use um
+# laço while para remover todas as ocorrências de 'pastrami' e
+# sandwich_orders. Garanta que nenhum sanduíche de pastrami acabe em
+# finished_sandwiches.
+
+# 7.10 – Férias dos sonhos: Escreva um programa que faça uma enquete sobre as
+# férias dos sonhos dos usuários. Escreva um prompt semelhante a este: Se
+# pudesse visitar um lugar do mundo, para onde você iria? Inclua um bloco de
+# código que apresente os resultados da enquete.
