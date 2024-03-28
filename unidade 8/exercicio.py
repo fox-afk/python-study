b0035416143d35e7fb14d2399b027eec3b232b93
Bruno Brito <72562028+fox-afk@users.noreply.github.com>
2024-03-28 08:25:33 -0300

    att

--- unidade 8/exercicio.py
+++ unidade 8/exercicio.py
@@ -55,3 +55,53 @@ def describe_city(type_cidade,type_pais):
 describe_city('são paulo','brasil')
 describe_city('vancouver','canada')
 describe_city('moscou','russia')
+
+# 8.6 – Nomes de cidade: Escreva uma função chamada city_country() que
+#aceite o nome de uma cidade e seu país. A função deve devolver uma string
+#formatada assim: "Santiago, Chile"
+#Chame sua função com pelo menos três pares cidade-país e apresente o valor
+#devolvido.
+
+#typecidade = input("Digite o nome da cidade: ")
+#typepais = input("Digite o nome do pais: ")
+#def city_country(typecidade,typepais):print(typecidade,typepais)
+#city_country(typecidade,typepais)
+
+#8.7 – Álbum: Escreva uma função chamada make_album() que construa um
+#dicionário descrevendo um álbum musical. A função deve aceitar o nome de um
+#artista e o título de um álbum e deve devolver um dicionário contendo essas
+#duas informações. Use a função para criar três dicionários que representem
+#álbuns diferentes. Apresente cada valor devolvido para mostrar que os
+#dicionários estão armazenando as informações do álbum corretamente.
+#Acrescente um parâmetro opcional em make_album() que permita armazenar
+#o número de faixas em um álbum. Se a linha que fizer a chamada incluir um
+#valor para o número de faixas, acrescente esse valor ao dicionário do álbum.
+#Faça pelo menos uma nova chamada da função incluindo o número de faixas
+#em um álbum.
+
+#album= input("Digite o album: ")
+#artista= input("Digite o nome do artista (banda): ")
+#def make_album(artista,album):print(artista,album)
+#make_album(artista,album)
+
+#8.8 – Álbuns dos usuários: Comece com o seu programa do Exercício 8.7.
+#Escreva um laço while que permita aos usuários fornecer o nome de um artista e
+#o título de um álbum. Depois que tiver essas informações, chame make_album()
+#com as entradas do usuário e apresente o dicionário criado. Lembre-se de incluir
+#um valor de saída no laço while.
+
+#8.9 – Mágicos: Crie uma lista de nomes de mágicos. Passe a lista para uma
+#função chamada show_magicians() que exiba o nome de cada mágico da lista.
+
+
+#8.10 – Grandes mágicos: Comece com uma cópia de seu programa do
+#Exercício 8.9. Escreva uma função chamada make_great() que modifique a
+#lista de mágicos acrescentando a expressão o Grande ao nome de cada
+#mágico. Chame show_magicians() para ver se a lista foi realmente modificada.
+
+#8.11 – Mágicos inalterados: Comece com o trabalho feito no Exercício 8.10.
+#Chame a função make_great() com uma cópia da lista de nomes de mágicos.
+#Como a lista original não será alterada, devolva a nova lista e armazene-a em
+#uma lista separada. Chame show_magicians() com cada lista para mostrar que
+#você tem uma lista de nomes originais e uma lista com a expressão o Grande
+#adicionada ao nome de cada mágico.
