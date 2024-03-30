61165d0d7fc6a295e06ce4bd5db6ac442f150b55
Bruno Brito <72562028+fox-afk@users.noreply.github.com>
2024-03-30 17:12:47 -0300

    att

---
+++ Curso Introdutório de Python 2/expressoes.py
@@ -0,0 +1,16 @@
+#1. Calcule o resto da divisão de 10 por 3
+
+def calculo():
+  print(  10 / 3)
+calculo()
+
+# tabuada do 13
+
+def tabuada13 ():
+  for i in range(1,11):
+    resultado=13*i
+    print(f"13 x",i, "=",resultado)
+tabuada13()
+#Davi não gosta de ir às aulas. Mas ele é obrigado a comparecer a pelo menos 75% delas. Ele quer saber
+#quantas aulas pode faltar, sabendo que tem duas aulas por semana, durante quatro meses. Ajude o Davinir!
+
